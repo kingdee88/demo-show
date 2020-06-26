f5e03c1f375e3cbdb9ef23577b275a602f0a6e76
董文昭 <>
2020-06-26 14:05:27 +0800

    固定资产配置计划 示例

---
+++ demo-front/src/views/asset-manage/fixed-assets-allocation/addEdit.vue
@@ -0,0 +1,343 @@
+<template>
+  <div class="user-edit">
+    <!-- Drawer抽屉 -->
+    <Drawer
+      :title="title"
+      v-model="visible"
+      width="720"
+      draggable
+      :mask-closable="type=='0'"
+    >
+      <div
+        :style="{maxHeight: maxHeight}"
+        class="drawer-content"
+      >
+        <div class="user-title">
+          <div class="info-title">年度资产购置明细</div>
+        </div>
+        <Form
+          ref="form"
+          :model="form"
+          :rules="formValidate"
+          label-position="top"
+        >
+          <Row
+            :gutter="32"
+            v-if="type=='2'"
+          >
+            <Col span="12">
+            <FormItem
+              label="拟购资产"
+              prop="username"
+            >
+              <Input
+                v-model="form.username"
+                autocomplete="off"
+                :maxlength="16"
+              >
+                <Button slot="append" icon="ios-git-merge"></Button>
+              </Input>
+            </FormItem>
+            </Col>
+            <Col span="12">
+            <FormItem
+              label="资产字典编码"
+              prop="password"
+            >
+              <Input
+                      v-model="form.username"
+                      autocomplete="off"
+                      :maxlength="16"
+              />
+            </FormItem>
+            </Col>
+          </Row>
+          <Row :gutter="32">
+            <Col span="12">
+            <FormItem
+              label="期初存量"
+              prop="nickname"
+            >
+              <Input v-model="form.nickname" />
+            </FormItem>
+            </Col>
+            <Col span="12">
+            <FormItem
+              label="本年拟购数量"
+              prop="email"
+            >
+              <Input v-model="form.email" />
+            </FormItem>
+            </Col>
+            <Col span="12">
+              <FormItem
+                      label="计量单位"
+                      prop="email"
+              >
+                <Input v-model="form.email" />
+              </FormItem>
+            </Col>
+            <Col span="12">
+              <FormItem
+                      label="预算单价（元）"
+                      prop="email"
+              >
+                <Input v-model="form.email" />
+              </FormItem>
+            </Col>
+            <Col span="12">
+              <FormItem
+                      label="资产可购置数量"
+                      prop="email"
+              >
+                <Input v-model="form.email" />
+              </FormItem>
+            </Col>
+            <Col span="12">
+              <FormItem
+                      label="采购预算（元）"
+                      prop="email"
+              >
+                <Input v-model="form.email" />
+              </FormItem>
+            </Col>
+            <Col span="12">
+              <FormItem
+                      label="资产配置标准"
+                      prop="email"
+              >
+                <Input v-model="form.email" />
+              </FormItem>
+            </Col>
+            <Col span="12">
+              <FormItem
+                      label="单价上限（元）"
+                      prop="email"
+              >
+                <Input v-model="form.email" />
+              </FormItem>
+            </Col>
+            <Col span="12">
+              <FormItem
+                      label="核算对象计量单位"
+                      prop="email"
+              >
+                <Input v-model="form.email" />
+              </FormItem>
+            </Col>
+            <Col span="12">
+              <FormItem
+                      label="本年核算对象数量"
+                      prop="email"
+              >
+                <Input v-model="form.email" />
+              </FormItem>
+            </Col>
+            <Col span="12">
+              <FormItem
+                      label="备注"
+              >
+                <Input type="textarea"
+                       v-model="form.description"
+                       :rows="4" />
+              </FormItem>
+            </Col>
+          </Row>
+          <Divider />
+        </Form>
+      </div>
+      <div
+        class="drawer-footer br"
+        v-show="type!='0'"
+      >
+        <Button
+          type="primary"
+          :loading="submitLoading"
+          @click="submit"
+        >提交</Button>
+        <Button @click="visible = false">取消</Button>
+      </div>
+    </Drawer>
+  </div>
+</template>
+
+<script>
+import { getAllRoleList, addUser, editUser } from "@/api/index";
+import {
+  validateUsername,
+  validateMobile,
+  validatePassword
+} from "@/libs/validate";
+import departmentTreeChoose from "@/views/my-components/xboot/department-tree-choose";
+import uploadPicInput from "@/views/my-components/xboot/upload-pic-input";
+import SetPassword from "@/views/my-components/xboot/set-password";
+export default {
+  name: "user",
+  components: {
+    departmentTreeChoose,
+    uploadPicInput,
+    SetPassword
+  },
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    },
+    data: {
+      type: Object
+    },
+    type: {
+      type: String,
+      default: "0"
+    }
+  },
+  data () {
+    return {
+      visible: this.value,
+      title: "",
+      passColor: "",
+      submitLoading: false,
+      maxHeight: 510,
+      form: {
+        addressArray: []
+      },
+      formValidate: {
+        // 表单验证规则
+        username: [
+          { required: true, message: "不能为空", trigger: "blur" },
+          { validator: validateUsername, trigger: "blur" }
+        ],
+        nickname: [
+          { required: true, message: "不能为空", trigger: "blur" }
+        ],
+        mobile: [
+          { required: true, message: "请输入手机号", trigger: "blur" },
+          { validator: validateMobile, trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { validator: validatePassword, trigger: "blur" }
+        ],
+        email: [
+          { required: true, message: "不能为空" }
+        ]
+      },
+      roleList: []
+    };
+  },
+  methods: {
+    init () {
+      this.getRoleList();
+    },
+    getRoleList () {
+      getAllRoleList().then(res => {
+        if (res.success) {
+          this.roleList = res.result;
+        }
+      });
+    },
+    handleSelectDepTree (v) {
+      this.form.departmentId = v;
+    },
+    changeAddress (v) {
+      this.form.address = JSON.stringify(this.form.addressArray);
+    },
+    changePass (v, grade, strength) {
+      this.form.passStrength = strength;
+    },
+    changeBirth (v, d) {
+      this.form.birth = v;
+    },
+    submit () {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          if (typeof this.form.birth == "object") {
+            this.form.birth = this.format(this.form.birth, "yyyy-MM-dd");
+          }
+
+          if (this.type == "1") {
+            // 编辑
+            this.submitLoading = true;
+            editUser(this.form).then(res => {
+              this.submitLoading = false;
+              if (res.success) {
+                this.$Message.success("操作成功");
+                this.$emit("on-submit", true);
+                this.visible = false;
+              }
+            });
+          } else {
+            // 添加
+            this.submitLoading = true;
+            addUser(this.form).then(res => {
+              this.submitLoading = false;
+              if (res.success) {
+                this.$Message.success("操作成功");
+                this.$emit("on-submit", true);
+                this.visible = false;
+              }
+            });
+          }
+        }
+      });
+    },
+    setCurrentValue (value) {
+      if (value === this.visible) {
+        return;
+      }
+      if (this.type == "1") {
+        this.title = "编辑";
+        this.maxHeight = Number(document.documentElement.clientHeight - 121) + "px";
+      } else if (this.type == "2") {
+        this.title = "添加";
+        this.maxHeight = Number(document.documentElement.clientHeight - 121) + "px";
+      } else {
+        this.title = "详情";
+        this.maxHeight = "100%";
+      }
+      // 清空数据
+      this.$refs.form.resetFields();
+      this.visible = value;
+    }
+  },
+  watch: {
+    value (val) {
+      this.setCurrentValue(val);
+    },
+    visible (value) {
+      this.$emit("input", value);
+    }
+  },
+  mounted () {
+    this.init();
+  }
+};
+</script>
+
+<style lang="less">
+@import "../../../styles/table-common.less";
+.drawer-content {
+  overflow: auto;
+}
+.drawer-content::-webkit-scrollbar {
+  display: none;
+}
+.user-title {
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+  .info-title {
+    font-size: 16px;
+    color: rgba(0, 0, 0, 0.85);
+    display: block;
+    margin-right: 16px;
+  }
+}
+.info-header {
+  font-size: 16px;
+  color: rgba(0, 0, 0, 0.85);
+  display: block;
+  margin-bottom: 16px;
+}
+</style>
+
---
+++ demo-front/src/views/asset-manage/fixed-assets-allocation/fixed-assets-allocation-index.vue
@@ -0,0 +1,608 @@
+<style lang="less">
+@import "../../../styles/table-common.less";
+</style>
+<template>
+  <div class="search">
+    <Card>
+      <Row
+        v-show="openSearch"
+        @keydown.enter.native="handleSearch"
+      >
+        <Form
+          ref="searchForm"
+          :model="searchForm"
+          inline
+          :label-width="70"
+        >
+            <Form-item
+              label="预算年度"
+              prop="sex"
+            >
+              <Select
+                v-model="searchForm.sex"
+                placeholder="预算年度"
+                clearable
+                style="width: 200px"
+              >
+                <Option value="0">2018</Option>
+                <Option value="1">2019</Option>
+                <Option value="2">2020</Option>
+              </Select>
+            </Form-item>
+          <Form-item
+            style="margin-left:-35px;"
+            class="br"
+          >
+            <Button
+              @click="handleSearch"
+              type="primary"
+              icon="ios-search"
+            >搜索</Button>
+            <Button @click="handleReset">重置</Button>
+          </Form-item>
+        </Form>
+      </Row>
+      <Row class="operation">
+        <Button
+          @click="add"
+          type="primary"
+          icon="md-add"
+        >添加</Button>
+        <Button
+          @click="delAll"
+          icon="md-trash"
+        >批量删除</Button>
+        <Button
+          type="dashed"
+          @click="openSearch=!openSearch"
+        >{{openSearch ? "关闭搜索" : "开启搜索"}}</Button>
+       <!-- <Button
+          type="dashed"
+          @click="openTip=!openTip"
+        >{{openTip ? "关闭提示" : "开启提示"}}</Button>-->
+      </Row>
+      <Row v-show="openTip">
+        <Alert show-icon>
+          已选择
+          <span class="select-count">{{selectCount}}</span> 项
+          <a
+            class="select-clear"
+            @click="clearSelectAll"
+          >清空</a>
+        </Alert>
+      </Row>
+      <Row>
+        <Table
+          :loading="loading"
+          border
+          :columns="columns"
+          :data="data"
+          sortable="custom"
+          @on-sort-change="changeSort"
+          @on-selection-change="showSelect"
+          ref="table"
+        ></Table>
+        <Table
+          :columns="exportColumns"
+          :data="exportData"
+          ref="exportTable"
+          style="display:none"
+        ></Table>
+      </Row>
+      <Row
+        type="flex"
+        justify="end"
+        class="page"
+      >
+        <Page
+          :current="searchForm.pageNumber"
+          :total="total"
+          :page-size="searchForm.pageSize"
+          @on-change="changePage"
+          @on-page-size-change="changePageSize"
+          :page-size-opts="[10,20,50]"
+          size="small"
+          show-total
+          show-elevator
+          show-sizer
+        ></Page>
+      </Row>
+    </Card>
+
+    <Modal
+      v-model="modalExportAll"
+      title="确认导出"
+      :loading="loadingExport"
+      @on-ok="exportAll"
+    >
+      <p>您确认要导出全部 {{total}} 条数据？</p>
+    </Modal>
+
+    <check-password
+      ref="checkPass"
+      @on-success="resetPass"
+    />
+
+    <addEdit
+      :data="form"
+      :type="showType"
+      v-model="showUser"
+      @on-submit="getUserList"
+    />
+  </div>
+</template>
+
+<script>
+import {
+  getUserListData,
+  enableUser,
+  disableUser,
+  deleteUser,
+  getAllUserData,
+  resetUserPass
+} from "@/api/index";
+import { validateMobile } from "@/libs/validate";
+import departmentChoose from "../../my-components/xboot/department-choose";
+import checkPassword from "@/views/my-components/xboot/check-password";
+import addEdit from "./addEdit.vue";
+export default {
+  name: "user-manage",
+  components: {
+    departmentChoose,
+    checkPassword,
+    addEdit
+  },
+  data () {
+    return {
+      height: 510,
+      showUser: false,
+      showType: "0",
+      loading: true,
+      openSearch: true,
+      openTip: false,
+      operationLoading: false,
+      loadingExport: true,
+      modalExportAll: false,
+      drop: false,
+      dropDownContent: "展开",
+      dropDownIcon: "ios-arrow-down",
+      selectCount: 0,
+      selectList: [],
+      dataDep: [],
+      searchKey: "",
+      searchForm: {
+        id: "",
+        nickname: "",
+        username: "",
+        departmentId: "",
+        mobile: "",
+        email: "",
+        sex: "",
+        type: "",
+        status: "",
+        pageNumber: 1,
+        pageSize: 10,
+        sort: "createTime",
+        order: "desc",
+        startDate: "",
+        endDate: ""
+      },
+      form: {},
+      columns: [
+        {
+          type: "selection",
+          width: 60,
+          align: "center",
+          fixed: "left"
+        },
+        {
+          title: "拟购资产",
+          key: "username",
+          minWidth: 125,
+          fixed: "left"
+        },
+        {
+          title: "资产字典编码",
+          key: "nickname",
+          minWidth: 125
+        },
+        {
+          title: "期初存量",
+          key: "username",
+          minWidth: 125
+        },
+        {
+          title: "本年拟购数量",
+          key: "username",
+          minWidth: 125
+        },
+        {
+          title: "预算单价(元)",
+          key: "username",
+          minWidth: 125
+        },
+        {
+          title: "资产可购置数量",
+          key: "username",
+          minWidth: 160
+        },
+        {
+          title: "采购预算(元)",
+          key: "username",
+          minWidth: 125
+        },
+        {
+          title: "资产配置标准",
+          key: "username",
+          minWidth: 125
+        },
+        {
+          title: "计量单位",
+          key: "username",
+          minWidth: 125
+        },
+        {
+          title: "预算单价（元）",
+          key: "username",
+          minWidth: 140
+        },
+        {
+          title: "资产可购置数量",
+          key: "username",
+          minWidth: 160
+        },
+        {
+          title: "采购预算（元）",
+          key: "username",
+          minWidth: 160
+        },
+        {
+          title: "资产配置标准",
+          key: "username",
+          minWidth: 125
+        },
+        {
+          title: "单价上限（元）",
+          key: "username",
+          minWidth: 160
+        },
+        {
+          title: "核算对象计量单位",
+          key: "username",
+          minWidth: 160
+        },
+        {
+          title: "本年核算对象数量",
+          key: "username",
+          minWidth: 160
+        },
+        {
+          title: "备注",
+          key: "departmentTitle",
+          minWidth: 120
+        },
+      ],
+      exportColumns: [
+        {
+          title: "用户名",
+          key: "username"
+        },
+        {
+          title: "头像",
+          key: "avatar"
+        },
+        {
+          title: "所属部门ID",
+          key: "departmentId"
+        },
+        {
+          title: "所属部门",
+          key: "departmentTitle"
+        },
+        {
+          title: "手机",
+          key: "mobile"
+        },
+        {
+          title: "邮箱",
+          key: "email"
+        },
+        {
+          title: "性别",
+          key: "sex"
+        },
+        {
+          title: "用户类型",
+          key: "type"
+        },
+        {
+          title: "状态",
+          key: "status"
+        },
+        {
+          title: "删除标志",
+          key: "delFlag"
+        },
+        {
+          title: "创建时间",
+          key: "createTime"
+        },
+        {
+          title: "更新时间",
+          key: "updateTime"
+        }
+      ],
+      data: [],
+      exportData: [],
+      total: 0,
+      dictSex: this.$store.state.dict.sex
+    };
+  },
+  methods: {
+    init () {
+      this.getUserList();
+    },
+    handleSelectDepTree (v) {
+      this.form.departmentId = v;
+    },
+    handleSelectDep (v) {
+      this.searchForm.departmentId = v;
+    },
+    changePage (v) {
+      this.searchForm.pageNumber = v;
+      this.getUserList();
+      this.clearSelectAll();
+    },
+    changePageSize (v) {
+      this.searchForm.pageSize = v;
+      this.getUserList();
+    },
+    selectDateRange (v) {
+      if (v) {
+        this.searchForm.startDate = v[0];
+        this.searchForm.endDate = v[1];
+      }
+    },
+    getUserList () {
+      // 多条件搜索用户列表
+      this.loading = true;
+      getUserListData(this.searchForm).then(res => {
+        this.loading = false;
+        if (res.success) {
+          this.data = [];
+          this.total = 0;
+        }
+      });
+    },
+    handleSearch () {
+      this.searchForm.pageNumber = 1;
+      this.searchForm.pageSize = 10;
+      this.getUserList();
+    },
+    handleReset () {
+      this.$refs.searchForm.resetFields();
+      this.searchForm.pageNumber = 1;
+      this.searchForm.pageSize = 10;
+      this.selectDate = null;
+      this.searchForm.startDate = "";
+      this.searchForm.endDate = "";
+      this.selectDep = [];
+      this.searchForm.departmentId = "";
+      // 重新加载数据
+      this.getUserList();
+    },
+    changeSort (e) {
+      this.searchForm.sort = e.key;
+      this.searchForm.order = e.order;
+      if (e.order == "normal") {
+        this.searchForm.order = "";
+      }
+      this.getUserList();
+    },
+    getRoleList () {
+      getAllRoleList().then(res => {
+        if (res.success) {
+          this.roleList = res.result;
+        }
+      });
+    },
+    handleDropdown (name) {
+      if (name == "refresh") {
+        this.getUserList();
+      } else if (name == "reset") {
+        if (this.selectCount <= 0) {
+          this.$Message.warning("您还未选择要重置密码的用户");
+          return;
+        }
+        this.$refs.checkPass.show();
+      } else if (name == "exportData") {
+        if (this.selectCount <= 0) {
+          this.$Message.warning("您还未选择要导出的数据");
+          return;
+        }
+        this.$Modal.confirm({
+          title: "确认导出",
+          content: "您确认要导出所选 " + this.selectCount + " 条数据?",
+          onOk: () => {
+            this.$refs.exportTable.exportCsv({
+              filename: "用户数据"
+            });
+          }
+        });
+      } else if (name == "exportAll") {
+        this.modalExportAll = true;
+      } else if (name == "importData") {
+        this.$Modal.info({
+          title: "请获取完整版",
+          content: "支付链接: http://xpay.exrick.cn/pay?xboot"
+        });
+      }
+    },
+    exportAll () {
+      getAllUserData().then(res => {
+        this.modalExportAll = false;
+        if (res.success) {
+          this.exportData = res.result;
+          setTimeout(() => {
+            this.$refs.exportTable.exportCsv({
+              filename: "用户全部数据"
+            });
+          }, 1000);
+        }
+      });
+    },
+    resetPass () {
+      this.$Modal.confirm({
+        title: "确认重置",
+        content:
+          "您确认要重置所选的 " +
+          this.selectCount +
+          " 条用户数据密码为【123456】?",
+        loading: true,
+        onOk: () => {
+          let ids = "";
+          this.selectList.forEach(function (e) {
+            ids += e.id + ",";
+          });
+          ids = ids.substring(0, ids.length - 1);
+          resetUserPass({ ids: ids }).then(res => {
+            this.$Modal.remove();
+            if (res.success) {
+              this.$Message.success("操作成功");
+              this.clearSelectAll();
+              this.getUserList();
+            }
+          });
+        }
+      });
+    },
+    showDetail (v) {
+      // 转换null为""
+      for (let attr in v) {
+        if (v[attr] == null) {
+          v[attr] = "";
+        }
+      }
+      let str = JSON.stringify(v);
+      let data = JSON.parse(str);
+      this.form = data;
+      this.showType = "0";
+      this.showUser = true;
+    },
+    add () {
+      this.showType = "2";
+      this.showUser = true;
+    },
+    edit (v) {
+      // 转换null为""
+      for (let attr in v) {
+        if (v[attr] == null) {
+          v[attr] = "";
+        }
+      }
+      let str = JSON.stringify(v);
+      let data = JSON.parse(str);
+      this.form = data;
+      this.showType = "1";
+      this.showUser = true;
+    },
+    enable (v) {
+      this.$Modal.confirm({
+        title: "确认启用",
+        content: "您确认要启用用户 " + v.username + " ?",
+        loading: true,
+        onOk: () => {
+          enableUser(v.id).then(res => {
+            this.$Modal.remove();
+            if (res.success) {
+              this.$Message.success("操作成功");
+              this.getUserList();
+            }
+          });
+        }
+      });
+    },
+    disable (v) {
+      this.$Modal.confirm({
+        title: "确认禁用",
+        content: "您确认要禁用用户 " + v.username + " ?",
+        loading: true,
+        onOk: () => {
+          disableUser(v.id).then(res => {
+            this.$Modal.remove();
+            if (res.success) {
+              this.$Message.success("操作成功");
+              this.getUserList();
+            }
+          });
+        }
+      });
+    },
+    remove (v) {
+      this.$Modal.confirm({
+        title: "确认删除",
+        content: "您确认要删除用户 " + v.username + " ?",
+        loading: true,
+        onOk: () => {
+          deleteUser({ ids: v.id }).then(res => {
+            this.$Modal.remove();
+            if (res.success) {
+              this.$Message.success("删除成功");
+              this.getUserList();
+            }
+          });
+        }
+      });
+    },
+    dropDown () {
+      if (this.drop) {
+        this.dropDownContent = "展开";
+        this.dropDownIcon = "ios-arrow-down";
+      } else {
+        this.dropDownContent = "收起";
+        this.dropDownIcon = "ios-arrow-up";
+      }
+      this.drop = !this.drop;
+    },
+    showSelect (e) {
+      this.exportData = e;
+      this.selectList = e;
+      this.selectCount = e.length;
+    },
+    clearSelectAll () {
+      this.$refs.table.selectAll(false);
+    },
+    delAll () {
+      if (this.selectCount <= 0) {
+        this.$Message.warning("您还未选择要删除的数据");
+        return;
+      }
+      this.$Modal.confirm({
+        title: "确认删除",
+        content: "您确认要删除所选的 " + this.selectCount + " 条数据?",
+        loading: true,
+        onOk: () => {
+          let ids = "";
+          this.selectList.forEach(function (e) {
+            ids += e.id + ",";
+          });
+          ids = ids.substring(0, ids.length - 1);
+          deleteUser({ ids: ids }).then(res => {
+            this.$Modal.remove();
+            if (res.success) {
+              this.$Message.success("删除成功");
+              this.clearSelectAll();
+              this.getUserList();
+            }
+          });
+        }
+      });
+    }
+  },
+  mounted () {
+    // 计算高度
+    this.height = Number(document.documentElement.clientHeight - 230);
+    this.init();
+  }
+};
+</script>
