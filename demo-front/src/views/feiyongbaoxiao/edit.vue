8a693375d73ba0ef40297e70509190cfcb45d203
chenkp <ccitkao@gmail.com>
2020-07-01 13:16:15 +0800

    修改bug

--- demo-front/src/views/feiyongbaoxiao/edit.vue
+++ demo-front/src/views/feiyongbaoxiao/edit.vue
@@ -19,8 +19,8 @@
             </div>
 
             <Card>
-                <p class="show-info">经办人部门：{{form.APPLY_DEPT_NAME}} 经办人：test11 申请日期：{{form.TIME_CREATE}}
-                    报销单编号：送审后自动生成</p>
+                <!-- <p class="show-info">经办人部门：{{form.APPLY_DEPT_NAME}} 经办人：test11 申请日期：{{form.TIME_CREATE}}
+                    报销单编号：送审后自动生成</p> -->
                 <Row type="flex" justify="space-between" :gutter="32">
                     <Col :xl="21" :xxl="16" style="border-right: 1px solid rgba(233, 232, 233, 0.6);">
                         <Form ref="form" :model="form" :label-width="135" :rules="formValidate">
